63ad66afa8a4d1f9e411eebe0b50c9fb32919810
Stacy <lundquist.stacy@gmail.com>
2020-11-28 17:24:35 -0800

    refactored tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,34 +3,9 @@ require "test_helper"
 describe UsersController do
   describe "auth_callback" do
     it "logs in an existing user and redirects to the root route" do
-      # Count the users, to make sure we're not (for example) creating
-      # a new user every time we get a login request
-      # start_count = User.count
-      #
-      # # Get a user from the fixtures
-      # user = users(:dan)
-      #
-      # # Tell OmniAuth to use this user's info when it sees
-      # # an auth callback from github
-      # OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-      #
-      # # Send a login request for that user
-      # # Note that we're using the named path for the callback, as defined
-      # # in the `as:` clause in `config/routes.rb`
-      # get auth_callback_path(:github)
-      #
-      # must_redirect_to root_path
-      #
-      # # Since we can read the session, check that the user ID was set as expected
-      # session[:user_id].must_equal user.id
-      #
-      # # Should *not* have created a new user
-      # User.count.must_equal start_count
-
       user = users(:dan)
       expect {
-        OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-        get auth_callback_path(:github)
+        perform_login(user)
       }.wont_change 'User.count'
 
       must_redirect_to root_path
@@ -38,9 +13,38 @@ describe UsersController do
     end
 
     it "creates an account for a new user and redirects to the root route" do
+      new_user = User.new(
+          username: 'aName',
+          provider: 'github',
+          email: 'name@namesplace.com',
+          uid: 123
+      )
+
+      expect {
+        perform_login(new_user)
+      }.must_change 'User.count', 1
+
+      must_redirect_to root_path
+      session[:user_id].must_equal User.last.id
     end
 
     it "redirects to the login route if given invalid user data" do
+      new_user = User.new(
+          username: nil,
+          provider: 'github',
+          email: 'someone@somewhere.com',
+          uid: 123
+      )
+
+      expect {
+        perform_login(new_user)
+      }.wont_change 'User.count'
+
+      must_redirect_to root_path
+      assert_nil(session[:user_id])
+
+      user = User.find_by(uid: new_user.uid, provider: new_user.provider)
+      assert_nil(user)
     end
   end
 end
